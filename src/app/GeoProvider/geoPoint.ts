baf060f11766adcb6ec6a6a31da4b060cf27d8df
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2021-03-18 04:41:24 +0100

    remove get and setter

--- src/app/GeoProvider/geoPoint.ts
+++ src/app/GeoProvider/geoPoint.ts
@@ -1,58 +1,49 @@
 export class GeoPoint {
-    private _latitude?: number = null;
-    private _longitude?: number = null;
-    private _altitude?: number = null;
-    private _accuracy?: number = null;
-    private _altitudeAccuracy?: number = null;
-    private _speed?: number = null;
-    private _heading?: number = null
-    private _timestamp?: number = null;
+    public latitude?: number = null;
+    public longitude?: number = null;
+    public altitude?: number = null;
+    public accuracy?: number = null;
+    public altitudeAccuracy?: number = null;
+    public speed?: number = null;
+    public heading?: number = null
+    public timestamp?: number = null;
   
     constructor(latitude=null, longitude=null, altitude=null, accuracy=null, altitudeAccuracy=null, heading=null, speed=null, timestamp=null){
-      this._latitude = latitude;
-      this._longitude = longitude;
-      this._altitude = altitude;
-      this._accuracy = accuracy;
-      this._altitudeAccuracy = altitudeAccuracy;
-      this._speed = speed;
-      this._heading = heading;
-      this._timestamp = timestamp;
+      this.latitude = latitude;
+      this.longitude = longitude;
+      this.altitude = altitude;
+      this.accuracy = accuracy;
+      this.altitudeAccuracy = altitudeAccuracy;
+      this.speed = speed;
+      this.heading = heading;
+      this.timestamp = timestamp;
     }
 
-    public get latitude() { return this._latitude; }
-    public get longitude() { return this._longitude; }
-    public get altitude() { return this._altitude; }
-    public get altitudeAccuracy() { return this._altitudeAccuracy; }
-    public get accuracy() { return this._accuracy; }
-    public get speed() { return this._speed; }
-    public get heading() { return this._heading; }
-    public get timestamp() { return this._timestamp; }
-
     public getLatitude(precision: number = 5){
-      return this._latitude != null ? this._latitude.toFixed(precision) : "NaN";
+      return this.latitude != null ? this.latitude.toFixed(precision) : "NaN";
     }
 
     public getLongitude(precision: number = 5){
-      return this._longitude != null ? this._longitude.toFixed(precision) : "NaN";
+      return this.longitude != null ? this.longitude.toFixed(precision) : "NaN";
     }
     
     public getAltitude(precision: number = 5){
-      return this._altitude != null ? this._altitude.toFixed(precision) : "NaN";
+      return this.altitude != null ? this.altitude.toFixed(precision) : "NaN";
     }
 
     public getSpeedToKm(precision: number = 3){
-      return this._speed != null ? (this._speed * 3.6).toFixed(precision) : "NaN";
+      return this.speed != null ? (this.speed * 3.6).toFixed(precision) : "NaN";
     }
 
     public getSpeedToM(precision: number = 3){
-      return this._speed != null ? this._speed.toFixed(precision) : "NaN";
+      return this.speed != null ? this.speed.toFixed(precision) : "NaN";
     }
 
     public getHeading(precision: number = 3){
-      return this._heading != null ? this._heading.toFixed(precision) : "NaN";
+      return this.heading != null ? this.heading.toFixed(precision) : "NaN";
     }
     
     public getAccuracy(precision: number = 3){
-      return this._accuracy != null ? this._accuracy.toFixed(precision) : "NaN";
+      return this.accuracy != null ? this.accuracy.toFixed(precision) : "NaN";
     }
   }
--- src/app/GeoProvider/geoTrack.ts
+++ src/app/GeoProvider/geoTrack.ts
@@ -1,22 +1,13 @@
 import { GeoPoint } from "./geoPoint";
 
 export class GeoTrack {
-    private _name?: string;
-    private _date?: number;
-    private _points?: Array<GeoPoint>;
-    
-    public get name(): string { return this._name; }
-    public set name(value: string) { this._name = value; }
+    public name?: string;
+    public timestamp?: number;
+    public points?: Array<GeoPoint>;
 
-    public get date(): number { return this._date; }
-    public set date(value: number) { this._date = value; }
-    
-    public get points(): Array<GeoPoint> { return this._points; }
-    public set points(value: Array<GeoPoint>) { this._points = value; }
-
-    constructor(name:string|null=null, date:number|null=null, points:Array<GeoPoint>|null=null){
-        this._date = (date == null) ? new Date().getTime() : date;
-        this._name = (name == null) ? "Track_"+this._date : name;
-        this._points = (points == null) ? new Array<GeoPoint>() : points;
+    constructor(name:string|null=null, timestamp:number|null=null, points:Array<GeoPoint>|null=null){
+        this.timestamp = (timestamp == null) ? new Date().getTime() : timestamp;
+        this.name = (name == null) ? "Track_"+this.timestamp : name;
+        this.points = (points == null) ? new Array<GeoPoint>() : points;
     }
 }
